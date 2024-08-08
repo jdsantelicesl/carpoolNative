8cee1ee17ec3e009f7753b844d998e49b16f51d8
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-08-07 18:13:28 -0700

    components only render when on screen

--- components/map/locFind.jsx
+++ components/map/locFind.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import axios from 'axios';
 
 const url = "http://192.168.1.23:5000/ride/locFind"
 
 const LocFind = ({ style, query }) => {
-    const [locations, setLocations] = useState(null)
+    const [locations, setLocations] = useState([])
     // placeholder for lat, long
     const lat = 38.5544
     const long = -121.7709
@@ -27,9 +27,14 @@ const LocFind = ({ style, query }) => {
     return (
         <View style={style}>
             {locations ? (
-                locations.map(location => (
-                    <Text key={location.id}>{location.displayName.text}</Text>
-                ))
+                <FlatList
+                    data={locations}
+                    renderItem={({ item }) =>
+                        <Text key={item.id}>{item.displayName.text}</Text>
+                    }
+                    keyExtractor={(item) => item.id}
+                    scrollEnabled={false} // Disable scrolling for FlatList
+                />
             ) : (
                 <Text>Loading.. </Text>
             )}
