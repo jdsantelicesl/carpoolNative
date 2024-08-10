6b1f9549c02bc4038ac2e079d1a88c91ccca6be1
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-08-10 14:42:01 -0700

    cleanup, formatting, and time input change

--- app/(tabs)/ride.jsx
+++ app/(tabs)/ride.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Dimensions, SafeAreaView, TextInput, Button, Alert, TouchableOpacity, ScrollView } from 'react-native';
+import {
+    Text, View, StyleSheet, Dimensions, SafeAreaView, TextInput, Button, Alert,
+    TouchableOpacity, ScrollView, TouchableWithoutFeedback, RefreshControl
+} from 'react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
-import RNPickerSelect from 'react-native-picker-select';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import Hr from '../../components/myComponents/hr';
 import axios from 'axios'; // Use this when we create a Flask server for data endpoints
 import OriginSlideUp from '../../components/map/OriginSlideUp';
 import LocFind from '../../components/map/locFind';
 
 // User id placeHolder. Replace after auth. The id is for test user
-const user_id = "66b43302b73820e5f0b813a6";
+const user_id = "66b573b5bd03d4f38b185868";
 const { width, height } = Dimensions.get('window');
 const vh = height * 0.01;
 const vw = width * 0.01;
@@ -22,21 +25,34 @@ const DayButton = ({ title, onPress, isSelected }) => (
     </TouchableOpacity>
 );
 
-const CustomButton = ({ onPress, title }) => (
-    <TouchableOpacity style={styles.submitButton} onPress={onPress}>
-        <Text style={styles.submitText}>{title}</Text>
-    </TouchableOpacity>
-);
-
 const ride = () => {
-    const [destination, setDest] = useState("");
-    const [from, setFrom] = useState("placeholder");
+    const [mapActive, setMapActive] = useState(false)
+    // Used for when user wants to refresh by pulling down page
+    const [refreshing, setRefreshing] = useState(false);
+
+    const [pickTimeVis, setTimeVis] = useState(true)
+    const [date, setDate] = useState(null)
+
+    const [destination, setDest] = useState(null);
+    const [from, setFrom] = useState(null);
     const [day, setDay] = useState(null); // 1 - 7, Sun - Sat
-    const [hour, setHour] = useState(0);
-    const [minute, setMinute] = useState(0)
+    const [hour, setHour] = useState("00");
+    const [minute, setMinute] = useState("00")
     const [amPm, setAmPm] = useState("AM");
     const days = ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'];
     const url = process.env.EXPO_PUBLIC_API_URL + "/ride/post"; // placeholder
+
+    const onRefresh = () => {
+        // display refreshing animation
+        setRefreshing(true);
+
+        // reset all values
+        setDest(null);
+        setFrom(null);
+        setDay(null);
+
+        setRefreshing(false);
+    }
 
     const handleLocClick = (locId) => {
         // this is the 'callback' function
@@ -47,40 +63,23 @@ const ride = () => {
         console.log(locId)
     }
 
-    // Create array for hours and minutes (to be used for time selector)
-    const hourItems = Array.from({ length: 13 }, (_, i) => ({
-        label: i < 10 ? `0${i}` : String(i),
-        value: i,
-    }));
-
-    // for minutes
-    const minuteItems = Array.from({ length: 61 }, (_, i) => ({
-        label: i < 10 ? `0${i}` : String(i),
-        value: i,
-    }));
-
-    const Submit = () => (
-        <View style={styles.submitContainer}>
-            <CustomButton
-                title="Submit"
-                onPress={(e) => handleSubmit(e)}
-            />
-        </View>
-    );
+    const handleDate = (date) => {
+        console.log('success')
+    }
 
     // Needs to be implemented, could be JSON object
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        let arrival = hour + minute/60;
 
-        if(amPm === "PM") {
-            if(hour != 12) {
+        let arrival = hour + minute / 60;
+
+        if (amPm === "PM") {
+            if (hour != 12) {
                 arrival += 12;
             }
         }
         else {
-            if(hour == 12) {
+            if (hour == 12) {
                 arrival -= 12;
             }
         }
@@ -105,33 +104,27 @@ const ride = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView>
+            { !mapActive && <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                        colors={['#2E74DD']}
+                    />
+                }
+            >
 
                 {/* Location Selection Component */}
 
                 <Text style={styles.title}>Find a Carpool</Text>
+
                 <View style={styles.inputContainer}>
-                    <View style={styles.inputWrapper}>
+                    <TouchableOpacity style={styles.inputWrapper} onPress={() => setMapActive(true)}>
                         <FontAwesome6 name="magnifying-glass" size={24} color="#6E6B6B" style={styles.icon} />
-                        <TextInput
-                            style={styles.locInput}
-                            placeholder="Where to?"
-                            placeholderTextColor="#6E6B6B"
-                            value={destination} 
-                            onChangeText={setDest}
-                        />
-                        <OriginSlideUp />
-                    </View>
-                    {/* <View style={styles.inputWrapper}>
-                        <FontAwesome6 name="magnifying-glass" size={24} color="#6E6B6B" style={styles.icon} />
-                        <TextInput
-                            style={styles.locInput}
-                            placeholder="Where from?"
-                            placeholderTextColor="#6E6B6B"
-                            value={from}
-                            onChangeText={setFrom}
-                        />
-                    </View> */}
+                        <Text style={styles.locInput}>
+                            {destination ? destination : 'Where to?'}
+                        </Text>
+                    </TouchableOpacity>
                 </View>
 
                 {/* Weekday Component */}
@@ -153,46 +146,30 @@ const ride = () => {
                 <View style={styles.timeContainer}>
                     <Text style={styles.subTitle}>Time:</Text>
                     <View style={styles.timePickerContainer}>
-                        {/* onChangeTime, create function for modal or inline time selection.
-                      * This current time button is interim placeholder
-                      */}
-
-                        <View style={styles.timePickerButton}>
-                            <RNPickerSelect
-                                onValueChange={(value) => setHour(value)}
-                                items={hourItems}
-                                style={pickerStyles}
-                                value={hour}
-                            />
-                            {/* RNPickerSelect can only take in a special format of style */}
-                            <Text style={{ fontSize: 2.5 * vh, color: "#6E6B6B", fontWeight: "bold" }}>:</Text>
-                            <RNPickerSelect
-                                onValueChange={(value) => setMinute(value)}
-                                items={minuteItems}
-                                style={pickerStyles}
-                                value={minute}
-                            />
-                        </View>
-
-                        <View style={styles.buttonsContainer}>
-                            <TouchableOpacity
-                                style={[styles.buttons, amPm === 'AM' && styles.activeButtons]}
-                                onPress={() => setAmPm('AM')}
-                            >
-                                <Text style={[styles.buttonsText, amPm === 'AM' && styles.activeButtonsText]}>AM</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={[styles.buttons, amPm === 'PM' && styles.activeButtons]}
-                                onPress={() => setAmPm('PM')}
-                            >
-                                <Text style={[styles.buttonsText, amPm === 'PM' && styles.activeButtonsText]}>PM</Text>
-                            </TouchableOpacity>
-                        </View>
+                        <TouchableOpacity style={styles.timePickerButton} onPress={() => setTimeVis(true)}>
+                            <Text>{hour}:{minute}</Text>
+                        </TouchableOpacity>
+                        <DateTimePickerModal
+                            isVisible={pickTimeVis}
+                            mode="time"
+                            onConfirm={(date) => handleDate(date)}
+                            onCancel={() => setTimeVis(false)}
+                        />
                     </View>
                 </View>
 
                 {/* Handle Submit Component */}
-                <Submit />
+                <View style={styles.submitContainer}>
+                    <TouchableOpacity
+                        onPress={(e) => handleSubmit(e)}
+
+                        // Apply disabled style conditionally
+                        style={[styles.submitButton, (destination && from && day && hour && minute && amPm) && { backgroundColor: '#2E74DD' }]}
+                        disabled={!(destination && from && day && hour && minute && amPm)} // Disable button press functionality
+                    >
+                        <Text style={[styles.submitText, (destination && from && day && hour && minute && amPm) && { color: 'black' }]}>Submit</Text>
+                    </TouchableOpacity>
+                </View>
 
                 <Hr style={styles.hr} />
 
@@ -201,7 +178,9 @@ const ride = () => {
                 <LocFind query={destination} handleLocClick={handleLocClick} />
 
                 {/* List of users (Make scrollable)*/}
-            </ScrollView>
+            </ScrollView> }
+
+            { mapActive && <OriginSlideUp setMapActive={setMapActive} /> }
         </SafeAreaView>
     );
 };
@@ -233,14 +212,14 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         alignItems: 'center',
-        marginTop: -2 * vh,
+        marginTop: -1 * vh,
     },
     inputWrapper: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: "#D9D9D9",
         borderRadius: 3 * vh,
-        marginBottom: 2 * vh,
+        marginBottom: 1.8 * vh,
         width: 90 * vw,
     },
     icon: {
@@ -248,7 +227,7 @@ const styles = StyleSheet.create({
     },
     locInput: {
         flex: 1,
-        height: 6.5 * vh,
+        paddingVertical: 1.6 * vh,
         fontSize: 2.5 * vh,
         fontWeight: "bold",
         color: "#6E6B6B",
--- components/map/OriginSlideUp.jsx
+++ components/map/OriginSlideUp.jsx
@@ -10,81 +10,85 @@ const { height, width } = Dimensions.get('window');
 const vh = height * 0.01;
 const vw = width * 0.01;
 
-const OriginSlideUp = ({ originLat, originLong }) => {
-	const [isModalVisible, setModalVisible] = useState(false);
+const OriginSlideUp = ({ setMapActive }) => {
+	const [isModalVisible, setModalVisible] = useState(true);
 	const pan = useState(new Animated.ValueXY())[0];
 	const [originText, setOriginText] = useState("");
+
+	const [isStretched, setIsStretched] = useState(false)
+
+	const [originLat, setLat] = useState(null);
+	const [originLong, setLong] = useState(null);
 
 	const handleChangeText = () => {
 		// Make this refer to Workflow #2, so it could let them query.
 	};
-	
+
 	const toggleModal = () => {
 		setModalVisible(!isModalVisible);
 	};
 
-	[originLat, setLat] = useState();
-	[originLong, setLong] = useState();
-
-
 	return (
 		<View style={styles.container}>
-			<TouchableOpacity onPress={() => toggleModal()}>
-				<FontAwesome6 name="map-pin" size={24} color="#6E6B6B" style={styles.mapPin} />
-			</TouchableOpacity>
-		<Modal
-			isVisible={isModalVisible}
-			onBackdropPress={toggleModal}
-			onBackButtonPress={toggleModal}
-
-			// Disabled Swipe, prioritizing figma user flow, figure out swipe later
-			
-			// swipeDirection= "down"
-			// swipeThreshold={500}
-			// onSwipeComplete={toggleModal}
-			// propagateSwipe={true}
-			animationInTiming = {100}
-			animationIn="fadeIn"
-			animationOut="fadeOut"
-			style={styles.modal}
-		>
 			<View style={styles.mapContainer}>
 				<MapScreen
-				onLatChange={setLat}
-				onLongChange={setLong}
+					onLatChange={setLat}
+					onLongChange={setLong}
 				/>
 			</View>
-			
-			<Animated.View style={[styles.modalContent, { transform: [{ translateY: pan.y }] }]}>
-				<TouchableOpacity onPress={toggleModal}>
-					{/* <Text style={styles.closeText}>Close</Text> */}
-					<FontAwesome6 name="circle-arrow-left" size={24} color="#000000" style={styles.xIcon} />
-				</TouchableOpacity>
-				<Text style={styles.subTitle}>Set your origin</Text>
-				<Text style={styles.subSubTitle}>Drag map to move pin</Text>
-				<Text style={styles.subSubTitle}>_________________________</Text>
-				<View style={styles.inputWrapper}>
-					<FontAwesome6 name="location-dot" size={24} color="#000000" style={styles.locDotIcon} />
-					<TextInput
-						style={styles.locInput}
-						placeholder= {String(originLat)}
-						placeholderTextColor="grey"
-						value={originText} 
-						onChangeText={() => handleChangeText()}
-					/>
-					<TouchableOpacity onPress={() => setOriginText("")}>
-						<FontAwesome6 name="xmark" size={24} color="#000000" style={styles.xIcon} />
+
+			<Modal
+				isVisible={isModalVisible}
+				onBackdropPress={toggleModal}
+				onBackButtonPress={toggleModal}
+
+				// Disabled Swipe, prioritizing figma user flow, figure out swipe later
+
+				swipeDirection={["down", "up"]}
+				swipeThreshold={500}
+				onSwipeComplete={toggleModal}
+				propagateSwipe={true}
+				animationInTiming={100}
+				animationIn="fadeIn"
+				animationOut="fadeOut"
+				style={styles.modal}
+			>
+				
+				{ !isStretched && 
+				<Animated.View style={[styles.modalContent, { transform: [{ translateY: pan.y }] }]}>
+					<TouchableOpacity onPress={() => setMapActive(false)}>
+						{/* <Text style={styles.closeText}>Close</Text> */}
+						<FontAwesome6 name="circle-arrow-left" size={24} color="#000000" style={styles.xIcon} />
 					</TouchableOpacity>
-				</View>
-				<TouchableOpacity>
-						<DestinationSlideUp
-						originLat = {originLat}
-						originLong = {originLong}
+					<Text style={styles.subTitle}>Set your origin</Text>
+					<Text style={styles.subSubTitle}>Drag map to move pin</Text>
+					<Text style={styles.subSubTitle}>_________________________</Text>
+					<View style={styles.inputWrapper}>
+						<FontAwesome6 name="location-dot" size={24} color="#000000" style={styles.locDotIcon} />
+						<TextInput
+							style={styles.locInput}
+							placeholder={String(originLat)}
+							placeholderTextColor="grey"
+							value={originText}
+							onChangeText={() => handleChangeText()}
 						/>
-				</TouchableOpacity>
-			</Animated.View>
-		</Modal>
-		
+						<TouchableOpacity onPress={() => setOriginText("")}>
+							<FontAwesome6 name="xmark" size={24} color="#000000" style={styles.xIcon} />
+						</TouchableOpacity>
+					</View>
+					<TouchableOpacity>
+						
+						<DestinationSlideUp
+							originLat={originLat}
+							originLong={originLong}
+						/>
+					</TouchableOpacity>
+				</Animated.View> }
+
+				{ /*isStretched && */ }
+				
+			</Modal>
+
 		</View>
 	);
 };
@@ -110,26 +114,26 @@ const styles = StyleSheet.create({
 		textAlign: 'left',
 	},
 	subTitle: {
-        color: "#000000",
+		color: "#000000",
 		justifyContent: "center",
-        textAlign: "center",
-        fontWeight: "bold",
-        fontSize: 20,
+		textAlign: "center",
+		fontWeight: "bold",
+		fontSize: 20,
 		marginTop: -1 * vh,
-    },
+	},
 	subSubTitle: {
-        color: "#6E6B6B",
-        textAlign: "center",
-        fontSize: 17,
-		marginBottom: -1 *vh,
-    },
+		color: "#6E6B6B",
+		textAlign: "center",
+		fontSize: 17,
+		marginBottom: -1 * vh,
+	},
 	mapContainer: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-    },
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
 	modal: {
 		justifyContent: 'flex-end',
 		margin: 0,
@@ -148,8 +152,8 @@ const styles = StyleSheet.create({
 	},
 	inputWrapper: {
 		flexDirection: 'row',
-        backgroundColor: '#D9D9D9',
-        borderRadius: 5 * vw,
+		backgroundColor: '#D9D9D9',
+		borderRadius: 5 * vw,
 		justifyContent: "center",
 		alignItems: "center",
 		width: 75 * vw,
@@ -157,35 +161,35 @@ const styles = StyleSheet.create({
 		// Bro how do you center this View
 		marginTop: 3.5 * vh,
 		marginHorizontal: 7 * vw,
-    },
+	},
 	locDotIcon: {
-        marginLeft: 4 * vw,
-    },
-	xIcon:{
+		marginLeft: 4 * vw,
+	},
+	xIcon: {
 		marginRight: 5 * vw,
 	},
 	locInput: {
-        flex: 1,
-        height: 6.5 * vh,
-        fontSize: 2.5 * vh,
-        fontWeight: "bold",
-        color: "black",
-        paddingLeft: 3 * vw,
-    },
+		flex: 1,
+		height: 6.5 * vh,
+		fontSize: 2.5 * vh,
+		fontWeight: "bold",
+		color: "black",
+		paddingLeft: 3 * vw,
+	},
 	confirmButton: {
 		backgroundColor: '#D9D9D9',
-        borderRadius: 5 * vw,
+		borderRadius: 5 * vw,
 		width: 75 * vw,
 		height: 4 * vh,
-        marginHorizontal: 7 * vw,
+		marginHorizontal: 7 * vw,
 		marginTop: 2 * vh,
 	},
 	confirmText: {
 		color: "#6E6B6B",
 		justifyContent: "center",
-        textAlign: "center",
-        fontWeight: "bold",
-        fontSize: 20,
+		textAlign: "center",
+		fontWeight: "bold",
+		fontSize: 20,
 		marginTop: 0.5 * vh,
 	},
 });
