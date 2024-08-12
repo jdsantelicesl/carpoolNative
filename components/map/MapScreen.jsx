b7e373f5040771882a9dcc5094d5b1a94a3fbf2a
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-08-11 17:01:16 -0700

    added polyline generation

--- components/map/MapScreen.jsx
+++ components/map/MapScreen.jsx
@@ -3,9 +3,13 @@ import { StyleSheet, View, Text } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import { FontAwesome6 } from '@expo/vector-icons';
 import polyline from '@mapbox/polyline';
+import axios from 'axios';
 // Refer to docs: https://github.com/react-native-maps/react-native-maps
 
-const MapScreen = ({ origin, dest }) => {
+const MapScreen = ({ origin, dest, newPolyline }) => {
+
+  const [polylineAvailable, setPolylineAvailable] = useState(false)
+  const [decodedPolyline, setPolyline] = useState(null);
 
   const [currentRegion, setCurrentRegion] = useState({
     latitude: 38.5655,
@@ -15,39 +19,55 @@ const MapScreen = ({ origin, dest }) => {
   });
 
   useEffect(() => {
-    if(origin.lat && origin.long && dest.lat && dest.long) {
+    if (origin.lat && origin.long && dest.lat && dest.long) {
       const setLat = (origin.lat + dest.lat) / 2;
       const setLong = (origin.long + dest.long) / 2;
-      const deltaLat = Math.abs(setLat - origin.lat)*4;
-      const deltaLong = Math.abs(setLong - origin.long)*3;
-      
+      const deltaLat = Math.abs(setLat - origin.lat) * 4;
+      const deltaLong = Math.abs(setLong - origin.long) * 3;
+
       setCurrentRegion({
         latitude: setLat,
         longitude: setLong,
         latitudeDelta: deltaLat,
         longitudeDelta: deltaLong,
       })
+
+      const send_data = {
+        origin: {
+          lat: origin.lat,
+          long: origin.long
+        },
+        destination: {
+          lat: dest.lat,
+          long: dest.long
+        }
+      }
+
+      const polyline_url = process.env.EXPO_PUBLIC_API_URL + "/ride/getRoute" // placeholder
+
+      axios.post(polyline_url, data = send_data)
+        .then(response => {
+          const encodedPolyline = response.data.routes[0].polyline.encodedPolyline
+
+          const decodedPoints = polyline.decode(encodedPolyline).map(point => ({
+            latitude: point[0],
+            longitude: point[1],
+          }));
+          setPolyline(decodedPoints);
+          setPolylineAvailable(true);
+        })
+        .catch((error) => {
+          console.log("error fetching polyline", error);
+        })
+
     }
   }, [origin, dest]);
 
-  
+
 
   const handleRegionChangeComplete = (region) => {
     setCurrentRegion(region);
   };
-
-  // Hardcoded for now
-  // Make this call fetchRoute(); instead
-  const encodedString = "ygfjFppodVDNRVBXAJdDp@FAZKj@nGGLK~BfJrBxB`@LNrCj@uHpn@q@fF_@vAcApBk@hAs@hA{ApCc@bAuE~NmAnEgB`GiDiB{@[eBg@oDq@_@Jo@O}C_@uAIyCA_CH_D^wCl@mMpDqJhD}HfDmDhAmAVwQ`DsAXeGfBMGsFfAaDXoADwBGSJ_CUgCg@iCy@kB{@{A}@uAcAaAiAo@kAm@aBSy@]cCe@wDU_A]o@Y]s@e@s@Qg@Cw@Jk@Vk@d@c@r@Y|@MdABtARjD@rAEbBWtBKn@[bAg@lAQ^?`@cBzBkOdQaEbF}G`Iq@~@qA`CaAdCmAdEYbB]~CMdC?zCJhDXtC|@xFxAlI\\`CjArG`@jDX~BRxBTzDLhDFhD?xk@?~OAnH?`QCtC@~JCzDU~P_@xNKbF]hPItH?hCBtGN~ATjFVbEn@tH`AxMzApR~@`LnBdQhEjd@fBfPxNxvAvJ~aArJp_A~AjNnJp_ArAxNdK`cAt@lIlA`QZxD`AxNr@tIxBfZd@dIjAnL|Inp@jM~~@z@fFvBpJnArF~@pFj@xE`@hF`AlNz@|GpZlxB~F`b@x@rEn@rClB`HbBzEnBnEzBdElAnB`CbDbc@dj@~KvNfAdBlCbEhB`DdBrDrBjFvAnEnA|Ez@hEx@bFf@`En@`Gp@jF|@nFXnAKd@dCrMtA`GjArDjAxClFrL^pAVnBF~AEdBKdA[|Ag@rAq@hAw@x@m@d@iAf@u@RyAJeELKFuNIe|@y@uJQUOoIw@aDq@mA_@W@AjMGbEm@CeF@k@Cu@Ka@KHw@F_A?kAC]GOMGsBE_DAAh@K??fA"
-
-  const decodedPoints = polyline.decode(encodedString).map(point => ({
-    latitude: point[0],
-    longitude: point[1],
-  }));
-
-  const lengthDecodedPointsArray = decodedPoints.length;
-  destinationLat = decodedPoints[0]["latitude"]
-  destinationLong = decodedPoints[0]["longitude"]
 
   return (
     <View style={styles.container}>
@@ -72,22 +92,24 @@ const MapScreen = ({ origin, dest }) => {
           description={"Desc"}
           tappable={true}
           color="blue"
-        /> }
+        />}
 
         {/* Destination Marker, renders if dest has coordinates */}
-        { dest.lat && dest.long && <Marker
+        {dest.lat && dest.long && <Marker
           coordinate={{ latitude: dest.lat, longitude: dest.long }}
           title={"Joe"}
           description={"Desc"}
           tappable={true}
           color="blue"
-        /> }
+        />}
 
-        <Polyline
-          coordinates={decodedPoints}
-          strokeColor="purple"
-          strokeWidth={3}
-        />
+        {polylineAvailable &&
+          <Polyline
+            coordinates={decodedPolyline}
+            strokeColor="purple"
+            strokeWidth={3}
+          />
+        }
       </MapView>
     </View>
   );
