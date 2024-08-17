44acc2425da7478895b947a42788199192f08f96
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-08-16 17:39:46 -0700

    get chats from backend

--- app/(tabs)/messages.jsx
+++ app/(tabs)/messages.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Text, View, SafeAreaView, StyleSheet, Dimensions, ScrollView, StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View, SafeAreaView, StyleSheet, Dimensions, ScrollView, StatusBar, FlatList } from 'react-native'
+import axios from 'axios'
 import Hr from '../../components/myComponents/hr';
 import Message from '../../components/myComponents/message';
 
@@ -8,6 +9,27 @@ const vh = height * 0.01;
 const vw = width * 0.01;
 
 const messages = () => {
+    const [listMessages, setMessages] = useState(null);
+
+    const url = process.env.EXPO_PUBLIC_API_URL; // placeholder
+    const user_id = process.env.EXPO_PUBLIC_USER_ID;
+
+    useEffect(() => {
+        const sendId = encodeURIComponent(user_id);
+        const sendUrl = url + `/message/getChats?client_id=${sendId}`;
+
+        axios.get(sendUrl)
+            .then(response => {
+                // response.data contains all rides with messages, therefore listMessages really contains rides.
+                // Having the whole ride object might be usefull for future features
+                setMessages(response.data);
+            })
+            .catch(error => {
+                console.log("error getting chats: ", error);
+            });
+    }, []);
+
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle="dark-content" />
@@ -16,25 +38,34 @@ const messages = () => {
                 <Hr style={styles.hr} />
 
                 {/** Example only. Should fetch data and use component to display data retrieved. */}
-                <Message 
-                    origin={"West Davis"} 
-                    destination={"Sacramento City College"} 
-                    day={2} 
-                    arrival={11.5}
-                    prevText={"Lets plan to leave by 7:10am tomorrow"}
+                <FlatList
+                    style={{marginTop: 0.5*vh}}
+                    scrollEnabled={false}
+                    data={listMessages}
+                    renderItem={({ item }) => (
+                        <Message
+                            origin={item.origins.short}
+                            destination={item.destination.short}
+                            day={item.day}
+                            arrival={item.arrival}
+                            prevText={item.messages[item.messages.length-1].content}
+                            rideData={item}
+                        />)
+                    }
+                    keyExtractor={item => item._id}
                 />
 
-                <Message 
-                    origin={"Sacramento"} 
-                    destination={"East Davis"} 
-                    day={2} 
-                    arrival={18.5} 
+                <Message
+                    origin={"Sacramento"}
+                    destination={"East Davis"}
+                    day={2}
+                    arrival={18.5}
                     prevText={"Headng back, anyone still up for Joe's Sandwiches"}
                 />
-                
-                <Message 
-                    origin={"Message from developers"} 
-                    prevText={"Make sure to know your carpool friends well"}
+
+                <Message
+                    origin={"Message from Developers :)"}
+                    prevText={"Remember to rate others after carpooling"}
                 />
 
             </ScrollView>
@@ -61,7 +92,7 @@ const styles = StyleSheet.create({
     },
     hr: {
         marginHorizontal: 2 * vw,
-        marginBottom: -1 * vh,
+        marginBottom: 0 * vh,
         height: 0.2 * vh,
         width: 96 * vw,
         backgroundColor: "black",
--- components/myComponents/message.jsx
+++ components/myComponents/message.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View, SafeAreaView, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
 import { FontAwesome6 } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
@@ -14,11 +14,11 @@ const convertDay = (day) => {
     return days[adjustedDay];
 };
 
-const Message = ({ style, origin, destination, day, arrival, prevText, people, profile, setChatVisible }) => {
+const Message = ({ style, origin, destination, day, arrival, prevText, rideData }) => {
     const dayOfWeek = convertDay(day);
     // sets hour, minute, amPm from 24h to 12h clock
     const hour = (Math.floor(arrival) > 12) ? (Math.floor(arrival) - 12) : Math.floor(arrival);
-    const roundMin = Math.round((arrival - Math.floor(arrival))*60);
+    const roundMin = Math.round((arrival - Math.floor(arrival)) * 60);
     const minute = roundMin < 10 ? `0${roundMin}` : roundMin;
     const amPm = (Math.floor(arrival) >= 12) ? "pm" : "am";
     const formattedTime = `${hour}:${minute}${amPm}`;
@@ -27,66 +27,70 @@ const Message = ({ style, origin, destination, day, arrival, prevText, people, p
     const [read, setRead] = useState(false);
 
     return (
-        <>
-        <View style={[style, styles.container]}>
-            {/* This is the grey circle, currently place holder for user profile */}
-            <FontAwesome style={styles.profile} name="user-circle" size={10 * vh} color="#D9D9D9" />
+        <View style={styles.componentCont}>
+            <View style={[style, styles.container]}>
+                {/* This is the grey circle, currently place holder for user profile */}
+                <FontAwesome style={styles.profile} name="user-circle" size={10 * vh} color="#D9D9D9" />
 
-            <View style={styles.content}>
+                <View style={styles.content}>
 
-                {/* Display Ride Information Origin -> Destination */}
-                <View style={styles.top}>
-                    <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
-                        {origin} <Text></Text> {destination && <FontAwesome6 name="arrow-right" size={16}/>} {destination} 
-                    </Text>
-                </View>
-                
-
-                <View style={styles.bottom}>
-                    {/* Sepparate the bottom into 3, day, arrival, and profile*/}
-                    <View style={styles.arrivalPromptContainer}>
-                        <Text style={styles.arrivalPrompt}>{day && dayOfWeek} {arrival && formattedTime} </Text>
+                    {/* Display Ride Information Origin -> Destination */}
+                    <View style={styles.top}>
+                        <Text style={styles.title} numberOfLines={1} ellipsizeMode="tail">
+                            {origin} <Text></Text> {destination && <FontAwesome6 name="arrow-right" size={16} />} {destination}
+                        </Text>
                     </View>
 
 
-                    <View>
-                        {/* Place holder. Will use this view to display user profiles. Users will be passes vie the people prop.
+                    <View style={styles.bottom}>
+                        {/* Sepparate the bottom into 3, day, arrival, and profile*/}
+                        <View style={styles.arrivalPromptContainer}>
+                            <Text style={styles.arrivalPrompt}>{day && dayOfWeek} {arrival && formattedTime} </Text>
+                        </View>
+
+
+                        <View>
+                            {/* Place holder. Will use this view to display user profiles. Users will be passes vie the people prop.
                         The prop will be a json object of their infor */}
 
-                        {/* I Added the profile on the top of the code instead, place holder is the circle,
+                            {/* I Added the profile on the top of the code instead, place holder is the circle,
                             If that is an option, we can delete this place holder. */}
+                        </View>
+
                     </View>
 
-                </View>  
+                    {/* Previous Text */}
+                    <View>
+                        <Text
+                            numberOfLines={1}
+                            ellipsizeMode="tail"
+                            style={styles.prevText}
+                        >
+                            {prevText}
+                        </Text>
+                    </View>
 
-                {/* Previous Text */}
-                <View> 
-                    <Text 
-                        numberOfLines={1}
-                        ellipsizeMode="tail"
-                        style={styles.prevText}
-                    >
-                        {prevText}
-                    </Text> 
                 </View>
 
-            </View>
-
-            <TouchableOpacity style={styles.button} onPress={() => alert("yo")}>
+                <TouchableOpacity style={styles.button} onPress={() => alert("yo")}>
                     <FontAwesome6 style={styles.arrow} name="chevron-right" size={20} color="black" />
-            </TouchableOpacity>
-            
+                </TouchableOpacity>
+
+            </View>
+            <Hr style={styles.hr} />
         </View>
-        <Hr style={styles.hr} />
-        </>
     )
 }
 
 export default Message;
 
 const styles = StyleSheet.create({
+    componentCont: {
+        height: 14*vh,
+    },
     container: {
-        marginVertical: 2 * vh,
+        marginTop: 1 * vh,
+        marginBottom: 2*vh,
         marginHorizontal: 2.5 * vw,
         flexDirection: "row",
     },
@@ -106,16 +110,16 @@ const styles = StyleSheet.create({
         width: 60 * vw,
     },
     profile: {
-        
+
     },
-    button:{
+    button: {
         // backgroundColor: "red",
         marginLeft: -2 * vh,
     },
     arrow: {
         flex: 1,
         marginTop: 2 * vh,
-        
+
     },
     bottom: {
         flexDirection: "row",
@@ -134,11 +138,11 @@ const styles = StyleSheet.create({
         width: 69 * vw, // Nice
     },
     arrivalContainer: {
-        marginLeft: 2*vw,
+        marginLeft: 2 * vw,
     },
     hr: {
         marginHorizontal: 2 * vw,
-        marginVertical: -1 * vh,
+        marginVertical: 0 * vh,
         height: 0.2 * vh,
         width: 96 * vw,
         backgroundColor: "black",
